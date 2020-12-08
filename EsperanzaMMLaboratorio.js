e44d0b984ff5c2d4b66c20f493182b969c4d3a03
EspePop <75271403+EspePop@users.noreply.github.com>
2020-12-08 18:05:26 +0100

    Update EsperanzaMMLaboratorio.js

--- EsperanzaMMLaboratorio.js
+++ EsperanzaMMLaboratorio.js
@@ -3,12 +3,13 @@
    function puntuar(){
 
     var punt = document.getElementById("valoracion").value;
-
-       alert("Has valorado con " + punt + " puntos");
+    
+    alert("Has valorado con " + punt + " puntos");
    }
 
 
    function cuentaBancaria(){
+
     let num1= document.getElementById("Pais").placeholder;
     let num2= document.getElementById("IBAN").value;
     let num3= document.getElementById("Entidad").value;
@@ -16,21 +17,23 @@
     let num5= document.getElementById("DC").value;
     let num6= document.getElementById("Cuenta").value;
 
-       alert(`Le informamos que su cuenta bancaria es: \n ${num1} ${num2} ${num3} ${num4} ${num5} ${num6}`);
+    alert(`Le informamos que su cuenta bancaria es: \n ${num1} ${num2} ${num3} ${num4} ${num5} ${num6}`);
    }
+   
 
    function diaSemana(){
 
       const semana = ["Lunes", "Martes", "Miércoles", "Jueves", "Viernes", "Sábado", "Domingo"];
       let dia
       let date = document.getElementById("fecha").value;
-     
-        if (date === "2019-01-01"){      
-           dia = "Martes";
-        } 
-        else {
+       
+      if (date === "2019-01-01"){ 
+         dia = "Martes";
+      } 
+      else {
          let miDia = new Date(date); 
          dia = semana[miDia.getUTCDay()-1];
-        }
-        alert("La fecha seleccionada en el elemento fecha es un " + dia);
       }
+
+      alert("La fecha seleccionada en el elemento fecha es un " + dia);
+   }
